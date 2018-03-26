8dada0a1fb63096f560d3bfdcc0087fa76ef14dc
karadumanb <karadumanbaturay@gmail.com>
2018-03-26 17:41:53 +0300

    Add todos manually and Display it

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <todo-list v-bind:todos="todos"></todo-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+/* eslint-disable */ 
+import TodoList from './components/TodoList'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TodoList
+  },
+  //data function is used to return data for template
+  //if we want to use this in another component, we need to use v-bind in template
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,53 @@
+<template>
+<div>
+    <!--JavaScript expressions in Vue are enclosed in double curly brackets.-->
+    <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+    <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+    <div class='card todo-card' v-for="todo in todos">
+      <div class='card-body'>
+        <div class='card-title'>
+          {{ todo.title }}
+        </div>
+        <div class='card-text'>
+          {{ todo.project }}
+        </div>
+        <div class='card-text'>
+          <span class='right floated edit icon'>
+            <i class='edit icon'></i>
+          </span>
+        </div>
+      </div>
+      <div class='card-text ui bottom attached green basic button mx-auto' v-show="todo.done">
+        Completed
+      </div>
+      <div class='card-text ui bottom attached red basic button mx-auto' v-show="!todo.done">
+        Complete
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */ 
+export default {
+  name: 'TodoList',
+  //we declare properties to accept the data sent from other components
+  props: ['todos'],
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ .todo-card {
+ width: 25%; 
+ margin:10px;
+ }
+ .card {
+  border: 2px solid skyblue;
+  text-align: center;
+ }
+ .button {
+   width: 50%;
+   text-align: center;
+ }
+</style>
